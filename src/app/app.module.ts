56eec336d72e90a0af77f1f8df009452adac7213
EnesZulfikaroglu <69876585+EnesZulfikaroglu@users.noreply.github.com>
2023-12-24 23:17:52 +0300

    last commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FlightSearchComponent } from './components/flight-search/flight-search.component';
 import { FlightListComponent } from './components/flight-list/flight-list.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+  
 @NgModule({
   declarations: [
     AppComponent,
     FlightSearchComponent,
     FlightListComponent,
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/flight-search/flight-search.component.ts
+++ src/app/components/flight-search/flight-search.component.ts
@@ -1,14 +1,19 @@
 // flight-search.component.ts
 
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { FlightService } from '../../services/flight.service';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-flight-search',
   templateUrl: './flight-search.component.html',
   styleUrls: ['./flight-search.component.css']
 })
-export class FlightSearchComponent {
+export class FlightSearchComponent implements OnInit {
+updateState() {
+  this.oneWay = !this.oneWay
+}
+
   @Output() searchEvent = new EventEmitter<any>();
 
   departureAirport: string = '';
@@ -17,16 +22,87 @@ export class FlightSearchComponent {
   returnDate: Date = new Date();
   oneWay: boolean = false;
   flights: any[] = []; // Burada flights özelliğini tanımladık
-
+  airports: any[] = []; // Burada flights özelliğini tanımladık
+  filteredFlights : any[] = [];
   constructor(private flightService: FlightService) {
     this.departureDate.setHours(0, 0, 0, 0);
     this.returnDate.setHours(0, 0, 0, 0);
   }
+  ngOnInit(): void {
+    this.searchAirports();
+    this.searchFlights();
+  }
+
+  signUpForm = new FormGroup({
+
+    departureAirports: new FormControl("", [
+      Validators.required,
+      Validators.minLength(5)
+    ])
+    ,
+
+    arrivalAirports: new FormControl("", [
+      Validators.required,
+      Validators.minLength(5)
+    ]),
+
+    departureDate: new FormControl("", [
+      Validators.required,
+    ]),
+
+    arrivalDate: new FormControl("", [
+      Validators.required,
+    ]),
+  
+  })
+    
+  onFormSubmit() {
+    console.log(this.signUpForm.value);
+    this.filteredFlights = []
+    var formValues = this.signUpForm.value;
+
+    
+    if(formValues.departureAirports && formValues.arrivalAirports){
+      for(var flight of this.flights){
+        var departureAirportCode = formValues.departureAirports.split(" ")[1]
+        var arrivalAirportCode = formValues.arrivalAirports.split(" ")[1]
+        if(flight.DepartureAirport == departureAirportCode && flight.arrivalAirport == arrivalAirportCode ){
+          this.filteredFlights.push(flight);
+          console.log("flght = ",this.filteredFlights)
+        }
+      }
+    }
+}
 
   searchFlights() {
     this.flightService.getFlights().subscribe((flights: any[]) => {
       this.flights = flights;
       this.searchEvent.emit(this.flights);
+      console.log(this.flights)
     });
+  }
+
+  searchAirports() {
+    this.flightService.getAllAirports().subscribe((airports: any[]) => {
+      this.airports = airports;
+      this.searchEvent.emit(this.airports);
+    });
+  }
+
+
+
+  myControl = new FormControl();
+  options = [
+    { value: 'option1', viewValue: 'Option 1' },
+    { value: 'option2', viewValue: 'Option 2' },
+    { value: 'option3', viewValue: 'Option 3' },
+  ];
+
+  filteredOptions = this.options;
+
+  applyFilter(filterValue: string) {
+    this.filteredOptions = this.options.filter(option =>
+      option.viewValue.toLowerCase().includes(filterValue.toLowerCase())
+    );
   }
 }
